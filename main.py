8275d4f61c437ec56f49284d9162899530ed511d
Учень <triskabo@gmail.com>
2026-04-09 09:06:39 +0300

    bot start

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ API_TOKEN = "8569352099:AAG68TPFnYk97NHwQhp46PIJQmY1pDio6is"
 dp=Dispatcher()
 
 @dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Привіт я твій перший бот")
 
+async def main():
+    await dp.start_polling(bot)
 
+if __name__ == '__main__':
+    asyncio.run(main))
 
 
