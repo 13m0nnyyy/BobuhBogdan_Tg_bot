931bdccff569c5be1e6ecbfd8ac18f26424d8077
Учень <triskabo@gmail.com>
2026-04-09 08:53:55 +0300

    abcdf

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-#abcd
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+API_TOKEN = "8569352099:AAG68TPFnYk97NHwQhp46PIJQmY1pDio6is"
+
 
 
 
