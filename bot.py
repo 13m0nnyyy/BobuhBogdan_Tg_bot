14d87190ac3b255c3d2fc57ba512aaf57a7dfdc7
Учень <triskabo@gmail.com>
2026-04-09 09:49:30 +0300

    bot commands

--- bot.py
+++ bot.py
@@ -3,26 +3,31 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 
-# Замініть токен на свій
+#токен
 API_TOKEN = "8569352099:AAG68TPFnYk97NHwQhp46PIJQmY1pDio6is"
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Обробник команди /start
+#обробник старту
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer("Привіт, я твій бот, напиши /help щоб дізнатись про мене🎉")
 
-# Обробник команди /help
+#команда help
 @dp.message(Command("help"))
 async def help_command(message: Message):
-    await message.answer("Ось що я вмію:\n/start - привітання👍\n/help - довідка✉️\n/about - про мене👌")
+    await message.answer("Ось що я вмію:\n/start - привітання👍\n/help - довідка✉️\n/about - про мене👌\n/joke - анекдот😂")
 
-# Обробник команди /about
+#команда about
 @dp.message(Command("about"))
 async def about_command(message: Message):
     await message.answer("Я був створений Бобухом Богданом📑")
+
+#команда joke
+@dp.message(Command("joke"))
+async def joke_command(message: Message):
+    await message.answer("Чому комп’ютер пішов у спортзал? Щоб прокачати свої байти!🤣😂")
 
 # Запуск
 async def main():
